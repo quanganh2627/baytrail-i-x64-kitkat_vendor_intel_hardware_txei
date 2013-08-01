aa09599d81c089705a34c86e88cc54b53181c4ed
Mark Allyn <mark.a.allyn@intel.com>
2013-08-01 07:41:20 -0700

    TXEI Middleware: fix compile issue in jb mr2
    BZ: 126827
    
    Change-Id: Id3032d347e6b32199a871da7dab90fdad5b98fdb
    Signed-off-by: Mark Allyn <mark.a.allyn@intel.com>
    Reviewed-on: http://android.intel.com:8080/121706
    Reviewed-by: buildbot <buildbot@intel.com>
    Reviewed-by: Shuster, Norvin <norvin.shuster@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Mangalampalli, Jayant <jayant.mangalampalli@intel.com>
    Tested-by: Mangalampalli, Jayant <jayant.mangalampalli@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- App/Android.mk
+++ App/Android.mk
@@ -21,32 +21,6 @@ LOCAL_MODULE_TAGS := eng
 include $(BUILD_EXECUTABLE)
 
 
-#############################
-#  CC6_UMIP_ACCESS_APP
-#
-include $(CLEAR_VARS)
-LOCAL_FORCE_STATIC_EXECUTABLE := true
-
-LOCAL_LIB_DIR := $(LOCAL_PATH)
-LOCAL_APP_DIR := sec_tool
-
-LOCAL_SRC_FILES += $(LOCAL_APP_DIR)/umip_access_app.c
-
-LOCAL_STATIC_LIBRARIES := CC6_TXEI_UMIP_ACCESS CC6_ALL_BASIC_LIB libcutils libc libtxei
-
-LOCAL_C_INCLUDES := $(LOCAL_LIB_DIR)/inc/ \
-$(TARGET_OUT_HEADERS)/libtxei             \
-$(LOCAL_PATH)/../Lib/sec_tool_lib/inc/
-
-LOCAL_CFLAGS := -DACD_WIPE_TEST
-
-LOCAL_MODULE := CC6_TXEI_UMIP_ACCESS_APP
-
-LOCAL_MODULE_TAGS := eng
-
-include $(BUILD_EXECUTABLE)
-
-
 #####################
 #  Security tools application (TXEI_SEC_TOOLS)
 #
@@ -58,7 +32,7 @@ LOCAL_APP_DIR := sec_tool
 
 LOCAL_SRC_FILES += $(LOCAL_APP_DIR)/sec_tools_app.c \
 
-LOCAL_STATIC_LIBRARIES := CC6_TXEI_UMIP_ACCESS CC6_ALL_BASIC_LIB libcutils libc libtxei libtxeimiscutils
+LOCAL_STATIC_LIBRARIES := CC6_TXEI_UMIP_ACCESS CC6_ALL_BASIC_LIB liblog libcutils libc libtxei libtxeimiscutils
 
 LOCAL_C_INCLUDES := $(LOCAL_LIB_DIR)/inc/ \
 $(TARGET_OUT_HEADERS)/libtxei             \
