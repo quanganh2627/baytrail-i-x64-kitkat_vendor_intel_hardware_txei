272773cd7ad62311d149e67de16349a46f4fd42a
Claude Brouat <claude.brouat@intel.com>
2013-12-02 10:33:45 +0100

    Unblock Cherrytrail compilation
    BZ: 159165
    
    Add Cherrytrail platform at the same place than baytrail.
    
    Change-Id: Id06d6ca4339f30e9545ee62d38572a918264c4df
    Signed-off-by: Claude Brouat <claude.brouat@intel.com>

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 # Build the components in this subtree
 # only for platforms with trusted execution engine
-ifeq ($(TARGET_BOARD_PLATFORM),baytrail)
+ifneq ($(filter $(TARGET_BOARD_PLATFORM),baytrail cherrytrail),)
 LOCAL_PATH:= $(call my-dir)
 include $(call all-subdir-makefiles)
 endif
