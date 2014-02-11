d8450d9b60d65d30fe0943c96716122af64a1f1d
Robert Chiras <robert.chiras@intel.com>
2014-02-11 16:24:08 +0200

    Revert "Baytrail: Update of widevine for Baytrail"
    This reverts commit 7cf3b4fec07d7c0f7eebcabd7ac9f5e1ff69c294.
    This patch breaks bayleybay target due to missing proper VPG drop.
    Hold on with this patch until we have the proper VPG drop.
    
    Change-Id: Iee3b9c425a9e7c9e0c5512cd71bb858f03c9feac
    Reviewed-on: https://android.intel.com/162589
    Reviewed-by: Chiras, Robert <robert.chiras@intel.com>
    Tested-by: Chiras, Robert <robert.chiras@intel.com>

--- meimm/Android.mk
+++ meimm/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 
-LOCAL_MODULE_TAGS:= debug eng tests optional
+LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := meimm.c
 LOCAL_MODULE    := libmeimm
 
@@ -14,11 +14,4 @@ LOCAL_SHARED_LIBRARIES := libcutils
 
 include $(BUILD_SHARED_LIBRARY)
 
-
-ALL_DEFAULT_INSTALLED_MODULES += $(SYMLINKS)
-
-# We need this so that the installed files could be picked up based on the
-# local module name
-ALL_MODULES.$(LOCAL_MODULE).INSTALLED := \
-    $(ALL_MODULES.$(LOCAL_MODULE).INSTALLED) $(SYMLINKS)
 
